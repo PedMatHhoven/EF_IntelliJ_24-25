57a2c9343f524be94351766d8c08d76c169961f9
PedMatHhoven <pedro.matias@gymhueck.de>
2024-09-30 09:08:32 +0200

    Verzweigung warmwerden

--- Code/_02_DatentypenVariablen/Übung.java
+++
@@ -1,7 +0,0 @@
-package _02_DatentypenVariablen;
-
-public class Übung {
-    public static void main(String[] args) {
-        System.out.println("Du meckerst heute viel!");
-    }
-}
---
+++ Code/_03_VerzweigungenSchleifen/warmwerden.java
@@ -0,0 +1,17 @@
+package _03_VerzweigungenSchleifen;
+
+import java.util.Scanner;
+
+public class warmwerden {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i=scan.nextInt();
+        if (i > 1) {
+            if (i > 3) {
+                i = 3;
+            }
+            else i *= 2;
+        }
+        System.out.println("i = " + i);
+    }
+}
