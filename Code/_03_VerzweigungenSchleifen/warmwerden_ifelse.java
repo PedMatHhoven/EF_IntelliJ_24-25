95fc95a8b76ae48a7537219825f814d80a48e633
PedMatHhoven <pedro.matias@gymhueck.de>
2024-09-30 22:17:38 +0200

    kleine Änderungen

--- Code/_03_VerzweigungenSchleifen/warmwerden_ifelse.java
+++ Code/_03_VerzweigungenSchleifen/warmwerden_ifelse.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class warmwerden_ifelse {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i=scan.nextInt();
+        System.out.println("Gib eine natürliche Zahl ein.");
+        int i = scan.nextInt();
+        System.out.println("i = " + i);
         if (i > 1) {
             if (i > 3) {
                 i = 3;
