a4cc70823e6d588ca42b10d7ef7007b23860b173
PedMatHhoven <pedro.matias@gymhueck.de>
2025-01-08 11:51:37 +0100

    erste eigene Klasse mit Zugriffsklasse

---
+++ Code/_07_OOP/Buch.java
@@ -0,0 +1,27 @@
+package _07_OOP;
+
+public class Buch {
+    //Attribute
+    private int Seitenzahl;
+    private String String, Titel, Autor;
+
+    //Methoden
+    //Konstruktor (gleicher Name wie Klasse)
+    public Buch(int seitenzahl, String titel, String autor) {
+        Seitenzahl = seitenzahl;
+        Titel = titel;
+        Autor = autor;
+    }
+    //setter = set-Methode
+    public void setTitel(String titel) {
+        Titel = titel;
+    }
+    //getter = get-Methode
+    public String getTitel() {
+        return Titel;
+    }
+    //kreative Extra-Methode - noch nicht fertig
+    public int Lesedauer(String name) {
+        return 0;
+    }
+}
---
+++ Code/_07_OOP/Zugriff.java
@@ -0,0 +1,10 @@
+package _07_OOP;
+
+public class Zugriff {
+    public static void main(String[] args) {
+        Buch Daniel = new Buch(250,"wadehaddeduddeda","Stefan Raab");
+        System.out.println(Daniel.getTitel());
+        Daniel.setTitel("WhDd");
+        System.out.println(Daniel.getTitel());
+    }
+}
